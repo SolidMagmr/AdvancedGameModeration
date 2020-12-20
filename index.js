d6180ffde7d14d0ea127592fecbe8f4e7191a301
SolidMagmr <solid.alexx@gmail.com>
2020-12-20 22:57:26 +0100

    Beginning of the project

---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./botconfig.json');
+
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+client.once('ready', () => {
+    console.log('Ready!');
+	client.user.setActivity('For foul language', {type: "WATCHING"});
+	Warns.sync();
+	
+});
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+	if (!command) return;
+
+	if (command.guildOnly && message.channel.type !== 'text') {
+		return message.reply('I can\'t execute that command inside DMs!');
+	}
+
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+    }
+})
+client.login(token);
