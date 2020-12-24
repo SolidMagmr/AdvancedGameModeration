ca15ab787a1eca34b2f327f912e19df26c903bcf
SolidMagmr <solid.alexx@gmail.com>
2020-12-24 16:39:21 +0100

    Little update to get everything started

---
+++ commands/Template.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	name: 'NameHere',
+	description: 'Template what to fill in', //Description of the command
+	aliasses: ['Test', 'DevHelp',], //If you want people to be able to use other names for the command write them here
+	cooldown: 5, //In seconds
+    guildOnly: true, //Only able to be used in the discord server itself
+    usage:'<Name argument here> <Another one here>', //If you want to set an correct usage of the command write them there, to keep everything conform put them between <>
+	execute(message, args) {  //Add code of the command here if you need 'Global' values, add them with a , seperated after the args and do the same in index.js
+		
+	},
+};
--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./botconfig.json');
-
+const Sequelize = require('sequelize');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+const sequelize = new Sequelize('database', 'user', 'password', {
+	host: 'localhost',
+	dialect: 'mysql',
+	logging: true,
+	operatorsAliases: false,
+	storage: 'database.sqlite'
+});
+
 client.once('ready', () => {
     console.log('Ready!');
 	client.user.setActivity('For foul language', {type: "WATCHING"});
-	Warns.sync();
 	
 });
+
+const cooldowns = new Discord.Collection();
 
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
@@ -42,6 +56,30 @@ client.on('message', message => {
 
 	if (!cooldowns.has(command.name)) {
 		cooldowns.set(command.name, new Discord.Collection());
-    }
-})
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+	try {
+		command.execute(message, args, sequelize);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
 client.login(token);
