ca7b8209243d1fdb3be92e28e893dd41ebc6e89b
SolidMagmr <solid.alexx@gmail.com>
2021-01-17 16:43:26 +0100

    Improved the new warn commands

--- commands/Delwarn.js
+++ commands/Delwarn.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-
+const fs = require('fs');
 
 module.exports = {
 	name: 'delwarn',
@@ -7,57 +7,132 @@ module.exports = {
 	aliasses: ['deletewarn', 'removewarn', 'destroywarn', 'unwarn'], 
 	cooldown: 5, 
     guildOnly: true, 
-    usage:'<Username>', 
+    usage:'<Username> or <Tag discord user>', 
 	execute(message, args, Warns) {  
 
-		if (!message.member.hasPermission("KICK_MEMBERS", false, true, false)) return message.channel.send("You do not have the required permissions");
+	if (!message.member.hasPermission("KICK_MEMBERS", false, true, false)) return message.channel.send("You do not have the required permissions");
 
-		var nameUser = args[0]
+    var username = args[0]
+    if(!username) {return message.channel.send(`Atleast one argument must be provided`)}
+    var totalfilter = args.slice(1).join(' ')
+
+ 
+
+    var warnobject = {
+
+    }
+    
+        
+        fs.readFile('./gameinfo.json', 'UTF-8', function(err,data){
+            if (err) throw err
+            var existing = JSON.parse(data)
+
+            var itemProcessed = 0;
+            var foundmatch = 'no' // because booleans suck in my expperience I just use string, not efficient but please fix this if you want :)
+
+            if(message.mentions.users.first()) {  //The case where no game is provided and it automates to a discord warn, use the message.mentions.first() to get warndata
+
+                existing.Games.forEach(function(element, index, array){
+                    if(element.tablename == 'discord'){
+
+						var discorduser = message.mentions.users.first()
+						var nodesc = 'No description given'
+						warnobject.extra = discorduser.id
+
+						const awaitfiltertitle = m=> m.author.id === message.author.id;
 		
-		var nodesc = 'No description given'
+						Warns.findAll({where: warnobject}).then(collected => {
 
-		const awaitfiltertitle = m=> m.author.id === message.author.id;
-		
-		Warns.findAll({where: {name:nameUser}}).then(collected => {
+							message.channel.send(`There were ${collected.length} entries found, which one of these do you want to remove?`)
 
-			message.channel.send(`There were ${collected.length} entries found, which one of these do you want to remove?`)
+							var foundwarns = new Discord.MessageEmbed()
+							.setColor('#34b5db')
+							.setTitle(`Warns found in the database of user ${discorduser.username}`)
+							collected.forEach(function(item, index) {
+							foundwarns.addField(`Warn nr.${index +1} with description:`, `${item.description || nodesc}`)
 
-			var foundwarns = new Discord.MessageEmbed()
-			.setColor('#34b5db')
-			.setTitle(`Warns found in the database of user ${nameUser}`)
-			collected.forEach(function(item, index) {
-				foundwarns.addField(`Warn nr.${index +1} with description:`, `${item.description || nodesc}`)
+						})
 
-			})
+						var selectedwarn = null
 
-			var selectedwarn = null
+						message.channel.send(foundwarns)
+						message.channel.send('Which ones of these would you like to remove?')
+						message.channel.awaitMessages(awaitfiltertitle, {max:1, time:30000}).then(found =>{
+						var integerr = parseInt(found.first().content)
 
-			message.channel.send(foundwarns)
-			message.channel.send('Which ones of these would you like to remove?')
-			message.channel.awaitMessages(awaitfiltertitle, {max:1, time:30000}).then(found =>{
-			var integerr = parseInt(found.first().content)
 
-			//console.log(typeof integerr)
-			//console.log(found.first().content)
 
-			if (typeof integerr === 'number') {
-			selectedwarn = integerr
-			message.channel.send(`You selected warn number ${integerr}`)
-			//console.log(selectedwarn)
-			collected[selectedwarn - 1].destroy()
+						if (typeof integerr === 'number') {
+							selectedwarn = integerr
+							message.channel.send(`You selected warn number ${integerr}`)
+							collected[selectedwarn - 1].destroy()
 		
 		
-		}
-			else return message.channel.send('Looks like the provided warn isn\'t a number or you didn\'t provide a numer in time')	
+						}
+						else return message.channel.send('Looks like the provided warn isn\'t a number or you didn\'t provide a numer in time')	
 			
-			})
+					})
 
-		
-				
+				}
+					)
+    
+                    }
+    
+                    itemProcessed++;
+                    if (itemProcessed === array.length) {
+                        if(foundmatch == 'no') {
+                            return message.channel.send(`The is no discord object created or you forgot to specify what game you want to warn on. See **m!help warn** for more info`) //Checks if discord isn't found
+                        }
+                        
+                    }
+    
+                })   
 
-		}
-			)
+            } else { //If it isn't a discord specific warn, search if there is a special case for a special game, otherwise execute the default
+
+                 foundmatch = 'yes'
+                          		
+					var nodesc = 'No description given'
+					warnobject.name = username
+
+					const awaitfiltertitle = m=> m.author.id === message.author.id;
 		
-		console.log(message + args)
+					Warns.findAll({where: warnobject}).then(collected => {
+
+						message.channel.send(`There were ${collected.length} entries found, which one of these do you want to remove?`)
+
+						var foundwarns = new Discord.MessageEmbed()
+						.setColor('#34b5db')
+						.setTitle(`Warns found in the database of user ${username}`)
+						collected.forEach(function(item, index) {
+						foundwarns.addField(`Warn nr.${index +1} with description:`, `${item.description || nodesc}`)
+
+						})
+
+					var selectedwarn = null
+
+					message.channel.send(foundwarns)
+					message.channel.send('Which ones of these would you like to remove?')
+					message.channel.awaitMessages(awaitfiltertitle, {max:1, time:30000}).then(found =>{
+					var integerr = parseInt(found.first().content)
+
+
+
+					if (typeof integerr === 'number') {
+						selectedwarn = integerr
+						message.channel.send(`You selected warn number ${integerr}`)
+						collected[selectedwarn - 1].destroy()
+	
+		
+					}
+					else return message.channel.send('Looks like the provided warn isn\'t a number or you didn\'t provide a numer in time')	
+			
+					})
+
+				}
+				)
+                
+            }      
+        })
 	},
 };
--- commands/Warn.js
+++ commands/Warn.js
@@ -1,65 +1,132 @@
 const Discord = require('discord.js');
-const fs = require('fs')
+const fs = require('fs');
+var minimist = require('minimist');
+const yargs = require('yargs/yargs');
 
 module.exports = {
 	name: 'warn',
-	description: 'Warns a user', //Description of the command
-	cooldown: 5, //In seconds
-    guildOnly: true, //Only able to be used in the discord server itself
-    usage:'<Username (Do **not** use the @ tag in discord)> <UserID or the discord tag> <Game> <desc> <use a flag -e at the end to add an extra field of information.>', //If you want to set an correct usage of the command write them there, to keep everything conform put them between <>
-	execute(message, args, Warns) {  //Add code of the command here if you need 'Global' values, add them with a , seperated after the args and do the same in index.js
-        var username = args[0]
-        var userIDD = args[1]
-        var gamee = args[2].toLowerCase()
-        var descsplit = args.slice(3).join(' ')
-        var extrasplit = descsplit.split('-e')
-        var desc = extrasplit[0]
-        var extraa = extrasplit[1]
-        var ByUser = message.author.username + '#' + message.author.discriminator
+	description: 'Warns a user', 
+	cooldown: 5, 
+    guildOnly: true, 
+    usage:'Basic form: <@discorduser> (This only works for discord but you **need** to have created a discord object with m!creategame) advanced form: <username> with flags -i <UserID> -g <game> -d <description> -e <extra field>',
+	execute(message, args, Warns) {  
+        
+    if (!message.member.hasPermission("KICK_MEMBERS", false, true, false)) return message.channel.send("You do not have the required permissions");
 
-        var setcycle = 1
-        var foundmatch = 'no'
+    var username = args[0]
+    if(!username) {return message.channel.send(`Atleast one argument must be provided`)}
+    var totalfilter = args.slice(1).join(' ')
+    var ByUser = message.author.username + '#' + message.author.discriminator
+    var argv = yargs(totalfilter).argv
 
-        if (!message.member.hasPermission("KICK_MEMBERS", false, true, false)) return message.channel.send("You do not have the required permissions");
+ 
+
+    var warnobject = {
+        name: username,
+        warnedBy: ByUser,
+
+    }
+    
+    if(argv.i) {
+        warnobject.userID = argv.i
+    }
+    if(argv.g) {
+        warnobject.game = argv.g.toLowerCase()
+    } 
+    if(argv.e) {
+        warnobject.extra = argv.e
+    }
+    if(argv.d) {
+        warnobject.description = argv.d
+    }
+
+        
 
         fs.readFile('./gameinfo.json', 'UTF-8', function(err,data){
             if (err) throw err
             var existing = JSON.parse(data)
 
-            existing.Games.forEach(function(element, index){
+            var itemProcessed = 0;
+            var foundmatch = 'no' // because booleans suck in my exerpeice I just use string, not efficient but please fix this if you want :)
 
-                if(element.tablename == gamee){
-                    foundmatch = 'yes'
-                    setcycle = element.Cycle
-                    //console.log(`The element cycle is ${element.Cycle}`)
-                   // console.log('Found a match')
+            if(!argv.g) {  //The case where no game is provided and it automates to a discord warn, use the message.mentions.first() to get warndata
 
-                    //console.log(foundmatch)
-                    if (foundmatch == 'no') return message.channel.send('The game provided wasn\'t found')
-                    Warns.create({
-                        name: username,
-                        userID: userIDD,
-                        game: gamee,
-                        cycle: setcycle,
-                        extra: extraa,
-                        description: desc,
-                        warnedBy: ByUser
+                existing.Games.forEach(function(element, index, array){
+                    if(element.tablename == 'discord'){
+
+                        console.log('Enterred the discord state')
+
+                        foundmatch = 'yes'
+                        if(!message.mentions.users.first()){return message.channel.send(`You forgot to mention a user or you didn't specify you want to warn someone on. See **m!help warn for more info**`)}
+                        var discorduser = message.mentions.users.first()
+
+                        warnobject.name = discorduser.username
+                        warnobject.userID = discorduser.discriminator
+                        warnobject.game = 'discord'
+                        warnobject.extra = discorduser.id
+                        warnobject.cycle = element.Cycle
+                        
+                        Warns.create(warnobject).then(function(){
+                            return message.channel.send(`${username} has been succesfully warned by ${message.author.username}`)
+                         }).catch(function(err){
+                            console.log(err)
+                            return message.channel.send('Oops, seems like I wasn\'t able to warn this person')
+                        })
+    
+                    }
+    
+                    itemProcessed++;
+                    if (itemProcessed === array.length) {
+                        if(foundmatch == 'no') {
+                            return message.channel.send(`The is no discord object created or you forgot to specify what game you want to warn on. See **m!help warn** for more info`) //Checks if discord isn't found
+                        }
+                        
+                    }
+    
+                })   
+
+            } else { //If it isn't a discord specific warn, search if there is a special case for a special game, otherwise execute the default
+
+                existing.Games.forEach(function(element, index, array){
+                    if(element.tablename == warnobject.game){
+                        foundmatch = 'yes'
+                        
+                        switch (argv.g) { //Cases of special games
+    
+                            
+    
+    
+                            
+                            
+                            default: //The warn system when there is no special case, user gives all arguments manually with no API functionality
+                            
+                             warnobject.cycle = element.Cycle
+    
+                                Warns.create(warnobject).then(function(){
+                                return message.channel.send(`${username} has been succesfully warned by ${message.author.username}`)
+                             }).catch(function(err){
+                                console.log(err)
+                                return message.channel.send('Oops, seems like I wasn\'t able to warn this person')
+                            })
+                        }
+    
+    
+                    }
+    
+                    itemProcessed++;
+                    if (itemProcessed === array.length) {
+                        if(foundmatch == 'no') {
+                            return message.channel.send(`The is no discord object created or you forgot to specify what game you want to warn on. See **m!help warn** for more info`) //Checks if discord isn't found
+                        }
+                    }
+    
+                }) 
+
+            }
+
             
-                    }).then(function(){
-                        //console.log("Succesfull warn")
-                        return message.channel.send(`${username} has been succesfully warned by ${message.author.username}`)
-                    }).catch(function(err){
-                        console.log(err)
-                        return message.channel.send('Oops, seems like I wasn\'t able to warn this person')
-                    })
 
-                }
-
-            })
+                  
         })
-
-
-        //paste it here
-       
 	},
 };
